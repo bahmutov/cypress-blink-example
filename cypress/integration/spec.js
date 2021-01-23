0ffd521d174342cb44006f86613f0e061467ce58
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-23 16:36:03 -0500

    add prettier

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,7 +3,7 @@
 describe('TodoMVC App', () => {
   it('shows loading element', () => {
     cy.visit('/?delay=500')
-    cy.get('.loading').should('be.visible')
+    cy.get('.loading', {timeout: 800}).should('be.visible')
     cy.get('.loading').should('not.be.visible')
     cy.get('li.todo').should('have.length', 2)
   })
